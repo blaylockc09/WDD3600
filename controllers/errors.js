6b7aa82f35c0c9a01f7c792c07fac23478bf90ef
Chris Blaylock <52610450+blaylockc09@users.noreply.github.com>
2021-02-17 22:12:10 -0500

    Chapter 8 and Chapter 9 Changes

--- controllers/errors.js
+++ controllers/errors.js
@@ -1,5 +1,5 @@
 // Handles 404 error and sets page title to page not found.
 exports.get404 = (req, res, next) => {
-    res.status(404).render('404', { pageTitle: 'Page Not Found' });
+    res.status(404).render('404', { pageTitle: 'Could Not Find Page', path: '/404'});
   };
   
